aef960860eeb7906fd973a4ac301313e10cf7bf1
Kiia Silvonen <silvonenkiia@gmail.com>
2025-01-08 17:30:14 +0200

    Heart rate calculation ready

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [age, setAge] = useState('')
+  const [lower, setLower] = useState(0)
+  const [upper, setUpper] = useState(0)
+
+  //Const to calculate the heart rate limits
+  const calculate = () => {
+    const lower = (220 - age) * 0.65
+    const upper = (220 - age) * 0.85
+
+    setLower(lower)
+    setUpper(upper)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.field}>Age</Text>
+      <TextInput
+        style={styles.field}
+        value={age}
+        onChangeText={text => setAge(text)}
+        keyboardType='decimal-pad'
+      />
+      <Text style={styles.field}>Limits</Text>
+      <Text style={styles.field}>{lower.toFixed()}-{upper.toFixed()}</Text>
+      <Button title="CALCULATE" onPress={calculate}></Button>
     </View>
   );
 }
@@ -13,8 +33,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
+    marginLeft: 10,
+    marginRight: 10
   },
+  field: {
+    marginBottom: 10
+  }
 });
